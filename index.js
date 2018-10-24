47a985dff918a8b559dd661a145b2a09b92cd2fa
ketan <kdhallu@gmail.com>
2018-10-25 00:35:40 +0530

    added a procfile.

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+var server = app.listen(8080, function () {
+    console.log("Listening on port %s...", server.address().port);
+});
+
+app.get("/", function(req, res) {
+    res.json({status:"true", message:"api accessible"});
+});
