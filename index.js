138a97084afa984c61c000c62bac3807a060a384
ketan <kdhallu@gmail.com>
2018-10-26 00:47:16 +0530

    adding google home modules

--- index.js
+++ index.js
@@ -2,6 +2,29 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 
+// Import the appropriate service and chosen wrappers
+const {
+  dialogflow,
+  Image,
+} = require('actions-on-google')
+
+// Create an app instance
+const dfInstance = dialogflow()
+
+
+// Register handlers for Dialogflow intents
+
+dfInstance.intent('Default Welcome Intent', conv => {
+  conv.ask('Hi, how is it going?')
+  conv.ask(`Here's a picture of a cat`)
+  conv.ask(new Image({
+    url: 'https://developers.google.com/web/fundamentals/accessibility/semantics-builtin/imgs/160204193356-01-cat-500.jpg',
+    alt: 'A cat',
+  }))
+})
+
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
