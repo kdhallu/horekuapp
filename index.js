59798f343c4b5a3ef25c62aaa9ef0fd7c5623569
ketan <kdhallu@gmail.com>
2018-10-26 01:21:14 +0530

    logger

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
 
 var server = app.listen(process.env.PORT || 8080, function () {
     console.log("Listening on port %s...", server.address().port);
 });
 
 app.get("/", function(req, res){
-	console.log('api called')
+	console.log('api called');
 	res.json({status:"true", message:"server running"})
 })
 
 app.post("/fulfillment", function(req, res) {
+	console.log('fullfilment called');
     res.json({status:"true", message:"server running"});
 });
