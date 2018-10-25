d29c0ec0e0475021c15e77a8a8b91eb6b7884730
ketan <kdhallu@gmail.com>
2018-10-26 01:57:31 +0530

    logger

--- index.js
+++ index.js
@@ -18,59 +18,21 @@ app.get("/", function(req, res){
 })
 
 app.post("/fullfilment", function(req, res) {
-	res.json({
-"fulfillmentText": "This is a text response",
-"fulfillmentMessages": [
-  {
-    "card": {
-      "title": "card title",
-      "subtitle": "card text",
-      "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
-      "buttons": [
-        {
-          "text": "button text",
-          "postback": "https://assistant.google.com/"
-        }
-      ]
-    }
-  }
-],
-"source": "example.com",
-"payload": {
-  "google": {
-    "expectUserResponse": true,
-    "richResponse": {
-      "items": [
-        {
-          "simpleResponse": {
-            "textToSpeech": "this is a simple response"
-          }
-        }
-      ]
-    }
-  },
-  "facebook": {
-    "text": "Hello, Facebook!"
-  },
-  "slack": {
-    "text": "This is a text response for Slack."
-  }
-},
-"outputContexts": [
-  {
-    "name": "projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/context name",
-    "lifespanCount": 5,
-    "parameters": {
-      "param": "param value"
-    }
-  }
-],
-"followupEventInput": {
-  "name": "event name",
-  "languageCode": "en-US",
-  "parameters": {
-    "param": "param value"
-  }
-}
-});
+	
+	let response = "This is a sample response from your webhook!";//Default response from the webhook to show it’s working
+	let responseObj={
+		 "fulfillmentText":response
+		,"fulfillmentMessages":[
+			{
+				"text": {
+					"text": [
+						"Hello I m Responding to intent"
+					]
+				}
+			}
+		]
+		,"source":""
+	}
+
+	res.json(responseObj);
 });
