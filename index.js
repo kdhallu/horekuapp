704d6967f6d614cb8a4b42a965b4748563b3c8f9
Dhallu, Ketan <dhallu@sky.optymyze.com>
2018-10-31 15:00:32 +0530

    changing

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ var bodyParser = require("body-parser");
 var app = express();
 const {dialogflow} = require('actions-on-google');
 
+let http = require('follow-redirects').https;
+
 const designFlow = dialogflow();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var server = app.listen(process.env.PORT || 8080, function () {
+var server = app.listen(process.env.PORT || 234, function () {
     console.log("Listening on port %s...", server.address().port);
 });
 
@@ -19,20 +21,30 @@ app.get("/", function(req, res){
 
 app.post("/fullfilment", function(req, res) {
 	
-	let response = "Webhook actually called. I hope you win hackathon";//Default response from the webhook to show it’s working
-	let responseObj={
-		 "fulfillmentText":response
-		,"fulfillmentMessages":[
-			{
-				"text": {
-					"text": [
-						"Hello I m Responding to intent"
-					]
-				}
-			}
-		]
-		,"source":""
-	}
+	let options = {
+		host: 'testing.optymyze.com',
+		path: '/optFarmIS08/services/jobs/Jobs1.User/trigger',
+		headers: {
+			'Content-Type': 'application/json',
+			'Environment-id': '59218',
+			'Authorization': 'Bearer pzKomcWjVbcriVT8r1sryth6Jc9A3AJlURzOiWApTVqhOMAmC1dH4M44gTH5Mxr0'
+		},
+		method: 'POST'
+	};
 
-	res.json(responseObj);
+	var req = http.request(options, function(httpResponse) {
+		console.log('Status: ' + httpResponse.statusCode);
+		console.log('Headers: ' + JSON.stringify(httpResponse.headers));
+		httpResponse.setEncoding('utf8');
+		httpResponse.on('data', function (body) {
+			res.json(body);
+		});
+	});
+
+	req.on('error', function(e) {
+		console.log('problem with request: ' + e);
+	});
+	// write data to request body
+	req.write(JSON.stringify({email: "pol@optymyze.com", period: "2020-05-01"}));
+	req.end();
 });
